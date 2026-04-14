a7f1105edf67f7897b1b459a4aa115962b54a779
YAMEEN INNAYAT <yameeninnayat@gmail.com>
2026-04-14 09:41:58 +0530

    Update q5.c

--- 3rd/q5.c
+++ 3rd/q5.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
+// fibonnaci for n 
 int main() {
-    int n, a = 0, b = 1, next, i;
+    int n, i;
+    int a = 0, b = 1, c;
 
-    printf("Enter the number of terms for fibonaci: ");
+    printf("Enter number: ");
     scanf("%d", &n);
 
-    // Fibonacci Sequence
     for(i = 1; i <= n; i++) {
         printf("%d ", a);
-        next = a + b;
+        c = a + b;
         a = b;
-        b = next;
+        b = c;
     }
 
     return 0;
