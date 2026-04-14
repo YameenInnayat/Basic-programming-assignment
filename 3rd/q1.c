a1f693a4d7fafeaf76e2684d57a0023dd70222b5
YAMEEN INNAYAT <yameeninnayat@gmail.com>
2026-04-14 09:08:13 +0530

    Create q1.c

---
+++ 3rd/q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// square pattern stars
+
+int main() {
+    int n, i, j;
+
+    printf("Enter size of square: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {        
+        for(j = 1; j <= n; j++) {    
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
