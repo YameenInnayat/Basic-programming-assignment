169b546de4243c5068953d3b616f95144384d86b
YAMEEN INNAYAT <yameeninnayat@gmail.com>
2026-04-19 14:45:39 +0530

    Update q1.c

--- 4th/q1.c
+++ 4th/q1.c
@@ -1,25 +1,72 @@
 #include <stdio.h>
-
+// Check armstrong number 2 to 4 digit numbers
 int main() {
-    int n=3, o=1, t=1, s=1, sum, a, b, c;
+    int u=1, o=1, t=1, s=1, sum, a, b, c, d, n;
 
     printf("Enter the number: ");
     scanf("%d", &sum);
 
-    a = sum / 100;
-    b = (sum % 100) / 10;
-    c = sum % 10;
-
-    for (int k=1; k<=n; k++) {
-        o = o * a;
-        t = t * b;
-        s = s * c;
+    if (sum >= 10 && sum <= 99)
+        n = 2;
+    else if (sum >= 100 && sum <= 999)
+        n = 3;
+    else if (sum >= 1000 && sum <= 9999)
+        n = 4;
+    else {
+        printf("Enter a 2, 3 or 4 digit number only\n");
+        return 0;
     }
 
-    if (sum == o+t+s)
-        printf("Is an Armstrong number\n");
-    else
-        printf("Not an Armstrong number\n");
+    if (n == 2) {
+        a = sum / 10;
+        b = sum % 10;
+
+        for (int k=1; k<=n; k++) {
+            o = o * a;
+            t = t * b;
+        }
+
+        if (sum == o+t)
+            printf("%d is an Armstrong number\n", sum);
+        else
+            printf("%d is not an Armstrong number\n", sum);
+    }
+
+    else if (n == 3) {
+        a = sum / 100;
+        b = (sum % 100) / 10;
+        c = sum % 10;
+
+        for (int k=1; k<=n; k++) {
+            o = o * a;
+            t = t * b;
+            s = s * c;
+        }
+
+        if (sum == o+t+s)
+            printf("%d is an Armstrong number\n", sum);
+        else
+            printf("%d is not an Armstrong number\n", sum);
+    }
+
+    else if (n == 4) {
+        a = sum / 1000;
+        b = (sum % 1000) / 100;
+        c = (sum % 100) / 10;
+        d = sum % 10;
+
+        for (int k=1; k<=n; k++) {
+            o = o * a;
+            t = t * b;
+            s = s * c;
+            u = u * d;
+        }
+
+        if (sum == o+t+s+u)
+            printf("%d is an Armstrong number\n", sum);
+        else
+            printf("%d is not an Armstrong number\n", sum);
+    }
 
     return 0;
 }
